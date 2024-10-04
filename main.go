30c500f71117465fcede2c7c21429e0dc9fdf3ee
gufranlazuardi <gufranlazuardi@gmail.com>
2024-10-05 01:13:07 +0700

    feat: gin

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"tiny-donate/user"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("test")
+	// dsn := "host=localhost user=macbook password=1234 dbname=tiny-donate port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	// // db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connection to database is good")
+
+	// var users []user.User
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("====================")
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}	
+
+func handler(c *gin.Context) {
+	dsn := "host=localhost user=macbook password=1234 dbname=tiny-donate port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var user []user.User
+	db.Find(&user)
+
+	c.JSON(http.StatusOK, user)
 }
+
+
+// controller
+// handler
+// service
+// repository
+// db
