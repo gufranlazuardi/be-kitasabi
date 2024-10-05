ba69f4116ae6d0df2536ca6bae3b121357315310
gufranlazuardi <gufranlazuardi@gmail.com>
2024-10-05 14:16:38 +0700

    feat: user repository

--- main.go
+++ main.go
@@ -2,39 +2,13 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"tiny-donate/user"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "host=localhost user=macbook password=1234 dbname=tiny-donate port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	// // db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is good")
-
-	// var users []user.User
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("====================")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}	
-
-func handler(c *gin.Context) {
 	dsn := "host=localhost user=macbook password=1234 dbname=tiny-donate port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
@@ -42,11 +16,14 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var user []user.User
-	db.Find(&user)
+	userRepository := user.NewReposistory(db)
+	user := user.User{
+		Name: "Gufran",
+	}
 
-	c.JSON(http.StatusOK, user)
-}
+	userRepository.Save(user)
+	
+}	
 
 
 // controller
